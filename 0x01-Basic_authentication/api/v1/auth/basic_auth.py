78b2f9a0bbd4ffe9f5f8c2fdcdb02422b9b7dbe3
G-omar-H <omarghazi12@gmail.com>
2024-07-16 22:27:56 +0100

    7. Basic - Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,4 +9,20 @@ class BasicAuth(Auth):
     """
     Base64 part of the Authorization header for a Basic Authentication
     """
-    pass
+
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """returns the Base64 part of the Authorization
+        header for a Basic Authentication
+
+        Args:
+            authorization_header (str): _description_
+
+        Returns:
+            str: _description_
+        """
+        if authorization_header is None or not isinstance(
+            authorization_header, str)\
+                or authorization_header[0: 6] != 'Basic ':
+            return None
+        return authorization_header[6:]
