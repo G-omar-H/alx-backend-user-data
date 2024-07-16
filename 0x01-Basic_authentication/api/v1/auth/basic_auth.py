7c2e578e6fcf082223073dc72654ff8cd5a8d469
G-omar-H <omarghazi12@gmail.com>
2024-07-16 23:43:38 +0100

    8. Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 basic_auth.py
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -26,3 +27,22 @@ class BasicAuth(Auth):
                 or authorization_header[0: 6] != 'Basic ':
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """
+        return the decoded value of a Base64 string base64_authorization_header
+
+        Args:
+            base64_authorization_header (str): _description_
+
+        Returns:
+            str: _description_
+        """
+        if base64_authorization_header is None or not isinstance(
+                base64_authorization_header, str):
+            return None
+        try:
+            return base64.b64decode(base64_authorization_header)
+        except base64.binascii.Error as err:
+            return None
