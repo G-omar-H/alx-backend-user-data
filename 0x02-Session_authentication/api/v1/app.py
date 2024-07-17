01132f267569b5eee5c023a00964b2db36820f6f
G-omar-H <omarghazi12@gmail.com>
2024-07-17 15:15:34 +0100

    1. Empty session

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -8,6 +8,7 @@ from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_auth import SessionAuth
 import os
 
 
@@ -18,6 +19,8 @@ auth = None
 
 if os.getenv('AUTH_TYPE') == 'basic_auth':
     auth = BasicAuth()
+elif os.getenv('AUTH_TYPE') == 'session_auth':
+    auth = SessionAuth()
 else:
     auth = Auth()
 
---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+session_auth.py
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    Sessionn authentification mechanism
+    """
+    pass
