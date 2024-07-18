a9716aa96af13aef8c748b50f918ff896dc19775
G-omar-H <omarghazi12@gmail.com>
2024-07-18 20:55:33 +0100

    19. Update password end-point

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -153,6 +153,9 @@ class Auth:
         try:
             user = self._db.find_user_by(reset_token=reset_token)
             hashed_password = _hash_password(password)
-            self._db.update_user(user.id, hashed_password=hashed_password)
+            self._db.update_user(
+                user.id,
+                hashed_password=hashed_password,
+                reset_token=None)
         except NoResultFound:
             raise ValueError
