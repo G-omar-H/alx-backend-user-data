cc9b0da8b2aa311de464fc2b5e5d873b44968151
G-omar-H <omarghazi12@gmail.com>
2024-07-18 15:41:26 +0100

    2. Find user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -7,8 +7,7 @@ from sqlalchemy import create_engine, inspect
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import InvalidRequestError
-from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import NoResultFound, InvalidRequestError
 from typing import TypeVar
 
 from user import Base, User
