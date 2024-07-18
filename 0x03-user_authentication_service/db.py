30f2c6f131ef6f5133a238dbd48a33f772b6e97d
G-omar-H <omarghazi12@gmail.com>
2024-07-18 13:51:32 +0100

    1. create user

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+"""
+DB module
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from typing import TypeVar
+
+from user import Base, User
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> TypeVar('User'):
+        """
+        save the new user to the database.
+
+        Args:
+            email (_type_): _description_
+        """
+        newUser = User(email=email, hashed_password=hashed_password)
+        session = self._session
+        session.add(newUser)
+        session.commit()
+        return newUser
