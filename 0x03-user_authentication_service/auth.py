75566ae3b2325ed1a15896ccab757fe6147c6459
G-omar-H <omarghazi12@gmail.com>
2024-07-18 18:48:03 +0100

    5. Register user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -43,5 +43,5 @@ class Auth:
             self._db.find_user_by(email=email)
             raise ValueError(f'User {email} already exists')
         except NoResultFound:
-            bcryptedPwd = self._hash_password(password)
-            return self._db.add_user(email, bcryptedPwd)
+            hashed_password = self._hash_password(password)
+            return self._db.add_user(email, hashed_password)
